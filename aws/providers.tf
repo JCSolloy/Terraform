fd96f08cbde6085bf62c43595f50b3b4d08bccbf
Juan Carlos Solloy <85032245+JCSolloy@users.noreply.github.com>
2024-11-08 20:33:31 +0000

    Test test

--- aws/providers.tf
+++ aws/providers.tf
@@ -10,4 +10,8 @@ provider "aws" {
     region                  = "us-west-2"
     shared_credentials_files = ["~/.aws/credentials"]
     profile = "vscode"
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> f2571c5 (Test)

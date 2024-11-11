e6810a5256cd3988fb0fbb1693c5ecb77bb5d106
Juan Carlos Solloy <85032245+JCSolloy@users.noreply.github.com>
2024-11-11 04:21:22 +0000

    Udemy course

--- aws/providers.tf
+++ aws/providers.tf
@@ -1,17 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-    region                  = "us-west-2"
-    shared_credentials_files = ["~/.aws/credentials"]
-    profile = "vscode"
-<<<<<<< HEAD
+  region                   = "us-west-2"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscode"
 }
-=======
-}
->>>>>>> f2571c5 (Test)

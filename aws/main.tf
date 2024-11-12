94042147d735f2828a1a17bce038dc48c3a8dec8
Juan Carlos Solloy <85032245+JCSolloy@users.noreply.github.com>
2024-11-12 01:08:02 +0000

    finish first course of terraform

--- aws/main.tf
+++ aws/main.tf
@@ -66,3 +66,34 @@ resource "aws_key_pair" "mtc-auth" {
   key_name = "mtckey"
   public_key = file("~/.ssh/mtckey.pub")
 }
+
+resource "aws_instance" "dev_node" {
+
+  instance_type = "t2.micro"
+  ami = data.aws_ami.server_ami.id
+  key_name = aws_key_pair.mtc-auth.id
+  security_groups = [aws_security_group.mtc-sg.id]
+  subnet_id = aws_subnet.mtc_public_subnet.id
+
+  user_data = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
+      hostname = self.public_ip,
+      user = "ubuntu",
+      identityfile = "~/.ssh/mtckey"
+    })
+
+    interpreter = var.host_os == "windows" ? ["powershell", "-Command"] : ["bash", "-c"]
+
+  }
+  
+}
